9e6e71f954698a4e0bcdd50e159ed9b9e89c8bd3
Bo-Hun Kim <godchiken@naver.com>
2021-07-19 09:26:33 +0900

    문법 정리

--- src/main/kotlin/TypeConversion.kt
+++ src/main/kotlin/TypeConversion.kt
@@ -1,14 +1,14 @@
 class TypeConversion {
     //기본 자료형들은 type cast function을 toXXXX() 형식으로 제공한다.
-    var intValue : Int = 54321
+    private var intValue : Int = 54321
     // 또한 명시적 형변환 (explicit type casting) 을 해야지만 할당이 가능하다.
     // 형변환시 오류를 막기위해 타 언어에서 제공하는 묵시적 형변환(Implicit type casting)을 제공하지 않는다.
     var toLongExampleForExplicitTypeCasting : Long = intValue.toLong()
-    var toByteExampleForExplicitTypeCasting : Byte = intValue.toByte();
+    var toByteExampleForExplicitTypeCasting : Byte = intValue.toByte()
 }
 
 fun main() {
-    var typeConversion = TypeConversion();
-    println(typeConversion.toLongExampleForExplicitTypeCasting);
-    println(typeConversion.toByteExampleForExplicitTypeCasting);
+    val typeConversion = TypeConversion()
+    println(typeConversion.toLongExampleForExplicitTypeCasting)
+    println(typeConversion.toByteExampleForExplicitTypeCasting)
 }
