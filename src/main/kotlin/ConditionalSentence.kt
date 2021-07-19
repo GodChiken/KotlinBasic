1589ae1a81886b015fbfd11b5c05b4f2a886babe
Bo-Hun Kim <godchiken@naver.com>
2021-07-19 10:14:12 +0900

    조건문 및 비교연산자

---
+++ src/main/kotlin/ConditionalSentence.kt
@@ -0,0 +1,15 @@
+class ConditionalSentence {
+    var a = 7
+    fun conditionalExample(){
+        if(a>10){
+            println("크다")
+        }else{
+            println("작다")
+        }
+    }
+}
+
+fun main() {
+    val conditionalSentence = ConditionalSentence()
+    conditionalSentence.conditionalExample()
+}
