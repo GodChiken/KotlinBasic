ae0f27417bdb9295321b1ef8324044840fcac6a4
Bo-Hun Kim <godchiken@naver.com>
2021-07-20 10:26:20 +0900

    흐름제어 중 특별한 부분 정리

--- src/main/kotlin/class/OnlyProperty.kt
+++ src/main/kotlin/class/OnlyProperty.kt
@@ -1,3 +1,22 @@
 package `class`
+
 // 클래스가 속성만 가진경우 다음과 같이 선언이 가능하다.
-class OnlyProperty(var name: String, var birthYear: Int)
+class OnlyProperty(var name: String, var birthYear: Int){
+    fun introduce(){
+        println("안녕하세요 ${name} 입니다. ${birthYear} 생이며 잘 부탁드립니다.")
+    }
+}
+
+fun main() {
+    val a = OnlyProperty("김보훈", 1988)
+    val b = OnlyProperty("배창현", 1986)
+    val c = OnlyProperty("황성인", 1888)
+
+    println("안녕하세요 ${a.name} 입니다. ${a.birthYear} 생이며 잘 부탁드립니다.")
+    println("안녕하세요 ${b.name} 입니다. ${b.birthYear} 생이며 잘 부탁드립니다.")
+    println("안녕하세요 ${c.name} 입니다. ${c.birthYear} 생이며 잘 부탁드립니다.")
+
+    a.introduce()
+    b.introduce()
+    c.introduce()
+}
