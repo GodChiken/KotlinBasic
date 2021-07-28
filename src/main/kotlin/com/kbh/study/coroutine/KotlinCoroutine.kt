e1d4e8cd6a292589c2e2c237d315e19d70782500
Bo-Hun Kim <godchiken@naver.com>
2021-07-28 10:47:27 +0900

    kotlin coroutine 실습 runBlocking ,lanch, async, await, cancel, delay, withTimeoutOrNull 예제 작성
    코틀린 튜토리얼 학습종료

--- src/main/kotlin/com/kbh/study/coroutine/KotlinCoroutine.kt
+++ src/main/kotlin/com/kbh/study/coroutine/KotlinCoroutine.kt
@@ -1,5 +1,9 @@
 package com.kbh.study.coroutine
 
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeoutOrNull
+
 /**
  * 코루틴은 코틀린에서만 쓰이는 것이 아닌 다양한 분야에서 쓰이고 있다.
  * 비동기적인 프로세스를 처리하기 위해 메인이 되는 루틴에서 서브 역활을 하는 코루틴을 두어 개발자가 제어함에 목적이 있다.
@@ -11,7 +15,43 @@ package com.kbh.study.coroutine
  * 튜토리얼성 2019년 강좌에서는 coroutine의 베이스 패키지가 kotlin 이었으나, kotlinx로 베이스 패키지가 변경됨을 확인했다.
  */
 fun main() {
-    //GlobalScope
+    // 전혀 실행되지 않음을 알 수 있다. 제어되는 스코프 및 프로그램이 종료가 되면 함께 종료가 된다.
+    /*scope.launch {
+        for (i in 1..10){
+            println(i)
+        }
+    }*/
+    /**
+     * 일정한 범위 내에서 실행이 되도록 보장하려면 blocking 을 수행해야한다
+     * cancel() 을 활용하여 코루틴을 2가지 경우로 따져서 종료시킬수 있다.
+     * 1. delay(), yield() 가 사용된 위치까지 수행하되고 종료
+     * 2. cancel()로 인하여 코루틴의 프로퍼티 isActive = false 가 되므로 이를 수동으로 확인하여 종료
+     */
+    /*runBlocking {
+        val launch = launch {
+            for (i in 1..10){
+                println(i)
+                delay(10)
+            }
+        }
+        val async = async {
+            println("async 종료")
+        }
+        println("async 대기")
+        println(async.await())
+        println("launch 대기")
+        launch.join()
+        println("launch 종료")
+    }*/
+    // 재시간내에 수행을 못하면 null 을 반환한다.
+    runBlocking {
+        val result = withTimeoutOrNull(50) {
+            for (i in 1..10) {
+                println(i)
+                delay(10)
+            }
+            "finish" // return
+        }
+        println(result)
+    }
 }
-
-class KotlinCoroutine
