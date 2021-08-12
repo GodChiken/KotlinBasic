4bef70939602671b3409238182b3db84046c9d55
GodChiken <godchiken@naver.com>
2021-08-12 15:32:57 +0900

    생성자 빠진부분 기록

--- src/main/kotlin/com/kbh/study/class/KotlinConstructor.kt
+++ src/main/kotlin/com/kbh/study/class/KotlinConstructor.kt
@@ -1,12 +1,14 @@
 package com.kbh.study.`class`
 
-class KotlinConstructor(var name: String, var birthYear: Int) {
+// 클래스 선언부에는 생성자 키워드가 생략이 가능하다.
+class KotlinConstructor constructor(var name: String, var birthYear: Int) {
     // 코틀린의 기본 생성자이며 파라미터 및 반환형이 없다.
     init {
         println("안녕하세요 ${this.name} 입니다. ${this.birthYear} 생이며 잘 부탁드립니다.")
     }
+
     // 보조 생성자 생성시 반드시 기본 생성자를 통해 속성을 초기회 해야하며, ': this' 를 기술한다.
-    constructor(name: String) : this(name, 1988){
+    constructor(name: String) : this(name, 1988) {
         println("보조 생성자사 생성됨")
     }
 }
