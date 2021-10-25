da981bb44235a546633d1a01339927daf0472675
GodChiken <godchiken@naver.com>
2021-10-25 11:12:51 +0900

    객체와 클래스 공부 진행중 - interface, abstract

---
+++ src/main/kotlin/com/kbh/study/class/all/Musician.kt
@@ -0,0 +1,9 @@
+package com.kbh.study.`class`.all
+
+abstract class Musician(val name: String, val activeForm : Int){
+    abstract fun instrument() : String
+}
+class Cellist(name: String, activeForm: Int, etc: String) : Musician(name, activeForm){
+    override fun instrument() = "String"
+}
+val ma = Cellist("Yo-Yo Ma", 1961, "some")
