e27b182419b88cba6e9b2a88184b1c37fba68ce6
GodChiken <godchiken@naver.com>
2021-11-18 16:45:43 +0900

    - 내부반복과 지연 연산  - 개요 및 filter, map 실습

--- src/main/kotlin/com/kbh/study/function/internal/iterate.kt
+++ src/main/kotlin/com/kbh/study/function/internal/iterate.kt
@@ -3,6 +3,11 @@ package com.kbh.study.function.internal
 /**
  * 사실 개인적으로 java8 stream api를 공부하고 실무에서도 활용한 터라 크게 거부감이 없으나
  * 코틀린에서 달라진점 추가된점을 확인해보려고한다.
+ *
+ * 자바에서 늘 해결 못했던점은 수천 개를 다루는 컬렉션의 경우 오버헤드의 이슈가 존재한다.
+ * 코틀린에서 시퀀스를 활용하여 개선하는데 시퀀스는 내부 반복자다.
+ *
+ * 시퀀스는 실행을 지연시키며 반복이 꼭 필요한 부분에서 반복을 진행한다.
  * */
 
 fun main() {
@@ -15,4 +20,17 @@ fun main() {
 
     numberList.filter { e -> e % 2 == 0}
         .forEach { e -> print("$e, ")}
+
+    val doubleList = mutableListOf<Int>()
+    for (i in numberList){
+        if(i % 2 == 0){
+            doubleList.add(i * 2)
+        }
+    }
+    println(doubleList)
+
+    val doubleListByFilterAndMap = numberList.filter { e -> e % 2 == 0 }
+        .map { e -> e * 2 }
+
+    println(doubleListByFilterAndMap)
 }
