1b5bd257936ca201aa7e0301a618ad4962a0a4a6
GodChiken <godchiken@naver.com>
2022-02-04 18:48:22 +0900

    - Duration API 라이브 샘플

--- src/main/kotlin/com/kbh/presentation/page2/IntroduceDurationAPI.kt
+++ src/main/kotlin/com/kbh/presentation/page2/IntroduceDurationAPI.kt
@@ -5,5 +5,13 @@ import kotlin.time.Duration.Companion.hours
 import kotlin.time.Duration.Companion.minutes
 
 fun main(){
-    //live coding!!
+    val day = 1.days
+    val hours = 3.hours
+    val minutes = 4.minutes
+
+    val result = day - hours - minutes
+    val result2 = 1.days - 3.hours - 5.minutes
+
+    println(result)
+    println(result2)
 }
